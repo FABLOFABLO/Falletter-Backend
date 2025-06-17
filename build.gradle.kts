02cfc1988fe2c97facc01df01b482c5c716ae4a2
eeuunn <haspring@dsm.hs.kr>
2025-06-17 21:15:14 +0900

    setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
+	kotlin("plugin.jpa") version "1.9.10"
 	id("org.springframework.boot") version "3.4.3"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -22,8 +23,41 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.3")
+
+	// developmentOnly
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	// validation
+	implementation("org.springframework.boot:spring-boot-starter-validation:2.7.3")
+
+	// jwt
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+	// security
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	// web
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// lombok
+	compileOnly("org.projectlombok:lombok")
+
+	// database
+	implementation("mysql:mysql-connector-java:8.0.28")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+	// jpa
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+	// annotation
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+	// mail
+	implementation ("org.springframework.boot', name: 'spring-boot-starter-mail', version: '3.0.5")
 }
 
 kotlin {
--- src/main/kotlin/com/example/falletterbackend/FalletterBackendApplication.kt
+++ src/main/kotlin/com/example/falletterbackend/FalletterBackendApplication.kt
@@ -1,8 +1,11 @@
 package com.example.falletterbackend
 
+import com.example.falletterbackend.common.security.TokenProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
+@EnableConfigurationProperties(TokenProperties::class)
 @SpringBootApplication
 class FalletterBackendApplication
 
