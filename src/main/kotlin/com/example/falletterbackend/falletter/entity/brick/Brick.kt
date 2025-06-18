623de2d5af3c5488cf255dbf924105407559bd6b
eeuunn <haspring@dsm.hs.kr>
2025-06-18 09:43:37 +0900

    add : brick 엔티티 추가

---
+++ src/main/kotlin/com/example/falletterbackend/falletter/entity/brick/Brick.kt
@@ -0,0 +1,27 @@
+package com.example.falletterbackend.falletter.entity.brick
+
+import com.example.falletterbackend.common.entity.EntityBase
+import com.example.falletterbackend.falletter.entity.letter.RewardType
+import com.example.falletterbackend.falletter.entity.question.Question
+import com.example.falletterbackend.falletter.entity.user.User
+import jakarta.persistence.*
+
+
+@Entity
+@Table(name = "tbl_brick")
+class Brick(
+    @Column(name = "brick_count", columnDefinition = "BIGINT", nullable = false)
+    val brickCount: Long,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "reward_type", columnDefinition = "VARCHAR(10)", nullable = false)
+    val rewardType: RewardType,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", columnDefinition = "BIGINT", nullable = false)
+    val user: User,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", columnDefinition = "BIGINT", nullable = false)
+    val question: Question
+) : EntityBase()
