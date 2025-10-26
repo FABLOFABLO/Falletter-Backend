860e54a018fdf064710650457ff665020f05de9a
eeuunn <haspring@dsm.hs.kr>
2025-10-26 21:27:47 +0900

    feat(item) : ItemRepository

---
+++ src/main/kotlin/com/example/falletterbackend/falletter/entity/item/repository/ItemRepository.kt
@@ -0,0 +1,17 @@
+package com.example.falletterbackend.falletter.entity.item.repository
+
+import com.example.falletterbackend.falletter.dto.item.response.ItemBrickGetCountResponse
+import com.example.falletterbackend.falletter.dto.item.response.ItemLetterGetCountResponse
+import com.example.falletterbackend.falletter.entity.item.Item
+import com.example.falletterbackend.falletter.entity.user.User
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface ItemRepository : JpaRepository<Item, Long> {
+    fun findBrickCountByUser(user: User): ItemBrickGetCountResponse
+
+    fun findLetterCountByUser(user: User): ItemLetterGetCountResponse
+
+    fun findEntityByUser(user: User): Item
+
+    fun findFirstByUserOrderByIdAsc(user: User): Item?
+}
