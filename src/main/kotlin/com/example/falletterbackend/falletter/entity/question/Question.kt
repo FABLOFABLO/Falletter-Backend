e9aed0c99c7d07fb8ed58a19525d744ee6921fa8
eeuunn <haspring@dsm.hs.kr>
2025-10-30 08:45:04 +0900

    Update Question.kt

--- src/main/kotlin/com/example/falletterbackend/falletter/entity/question/Question.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/entity/question/Question.kt
@@ -1,15 +1,14 @@
 package com.example.falletterbackend.falletter.entity.question
 
-import com.example.falletterbackend.common.entity.EntityBase
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.Table
-
+import jakarta.persistence.*
 
 @Entity
 @Table(name = "tbl_question")
 class Question(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
     @Column(name = "question", columnDefinition = "VARCHAR(255)")
     val question: String
-
-) : EntityBase()
+)
