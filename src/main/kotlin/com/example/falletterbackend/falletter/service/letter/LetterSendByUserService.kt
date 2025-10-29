f7aea3f09720de1ae58d287e5cdd34d2cfdf75bc
eeuunn <haspring@dsm.hs.kr>
2025-10-29 09:58:32 +0900

    Update LetterSendByUserService.kt

--- src/main/kotlin/com/example/falletterbackend/falletter/service/letter/LetterSendByUserService.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/service/letter/LetterSendByUserService.kt
@@ -1,29 +1,32 @@
 package com.example.falletterbackend.falletter.service.letter
 
 import com.example.falletterbackend.falletter.dto.letter.request.LetterSentRequest
+import com.example.falletterbackend.falletter.entity.item.repository.ItemRepository
 import com.example.falletterbackend.falletter.entity.letter.Letter
 import com.example.falletterbackend.falletter.entity.letter.repository.LetterRepository
+import com.example.falletterbackend.falletter.facade.user.UserFacade
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class LetterSendByUserService(
+    private val itemRepository: ItemRepository,
     private val letterRepository: LetterRepository,
+    private val userFacade: UserFacade
 ) {
     @Transactional
     fun execute(request: LetterSentRequest) {
-        val itemLetter = letterRepository.findByUser(request.sender) ?: throw IllegalStateException("보낼 수 있는 래터가 없습니다.")
+        val user = userFacade.getCurrentUser()
 
-        if (itemLetter.letterCount <= 0) { throw IllegalStateException("래터 수가 부족합니다.") }
+        if (!itemRepository.existsByUserAndLetterCountGreaterThan(user, 0)) {
+            throw IllegalStateException("보유한 래터가 없습니다.")
+        }
 
         val sendLetter = Letter(
             content = request.content,
             reception = request.reception,
-            sender = request.sender
+            sender = user
         )
         letterRepository.save(sendLetter)
-
-        itemLetter.changeLetterCount(-1)
-        letterRepository.save(itemLetter)
     }
 }
