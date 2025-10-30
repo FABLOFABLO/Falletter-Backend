d3750a76ba0628c1651818b4d4a5c54807f80f4c
eeuunn <haspring@dsm.hs.kr>
2025-10-30 14:03:22 +0900

    Update CommunityGetPostService.kt

--- src/main/kotlin/com/example/falletterbackend/falletter/service/communtiy/CommunityGetPostService.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/service/communtiy/CommunityGetPostService.kt
@@ -2,7 +2,7 @@ package com.example.falletterbackend.falletter.service.communtiy
 
 import com.example.falletterbackend.falletter.dto.community.response.CommunityPostsResponse
 import com.example.falletterbackend.falletter.entity.community.repository.CommunityRepository
-import jakarta.persistence.EntityNotFoundException
+import com.example.falletterbackend.falletter.exception.community.CommunityNotFoundException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -12,8 +12,8 @@ class CommunityGetPostService(
 ) {
     @Transactional
     fun execute(id: Long): CommunityPostsResponse {
-        val community =  communityRepository.findById(id)
-            .orElseThrow { (EntityNotFoundException("게시물 아이디를 찾을 수 없음 : $id")) }
+        val community = communityRepository.findById(id)
+            .orElseThrow { throw CommunityNotFoundException }
 
         return CommunityPostsResponse.from(community)
     }
