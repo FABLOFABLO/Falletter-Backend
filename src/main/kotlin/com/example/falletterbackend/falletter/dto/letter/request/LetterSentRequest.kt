9316a648bbbff53927d62f8bc815bfe9cba63684
eeuunn <haspring@dsm.hs.kr>
2025-11-05 21:55:54 +0900

    Update LetterSentRequest.kt

--- src/main/kotlin/com/example/falletterbackend/falletter/dto/letter/request/LetterSentRequest.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/dto/letter/request/LetterSentRequest.kt
@@ -1,8 +1,6 @@
 package com.example.falletterbackend.falletter.dto.letter.request
 
-import com.example.falletterbackend.falletter.entity.user.User
-
 data class LetterSentRequest(
     val content: String,
-    val reception: User,
+    val reception: Long,
 )
