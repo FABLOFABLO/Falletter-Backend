c0b383f59d91b09f5d2de5eadc9926ceda5c6ced
eeuunn <haspring@dsm.hs.kr>
2025-11-06 09:14:16 +0900

    Update LetterSentListService.kt

--- src/main/kotlin/com/example/falletterbackend/falletter/service/letter/LetterSentListService.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/service/letter/LetterSentListService.kt
@@ -2,6 +2,7 @@ package com.example.falletterbackend.falletter.service.letter
 
 import com.example.falletterbackend.falletter.dto.letter.response.LetterSentListResponse
 import com.example.falletterbackend.falletter.entity.letter.repository.LetterRepository
+import com.example.falletterbackend.falletter.exception.letter.LetterNotSendException
 import com.example.falletterbackend.falletter.facade.user.UserFacade
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -14,18 +15,24 @@ class LetterSentListService(
 ) {
     @Transactional(readOnly = true)
     fun execute(): List<LetterSentListResponse> {
-        val currentUser = userFacade.getCurrentUser()
-        val letters = letterBoxRepository.findAllBySender(currentUser)
+        val user = userFacade.getCurrentUser()
+        val letters = letterBoxRepository.findAllBySender(user)
 
-        return letters.map { letter ->
-            LetterSentListResponse(
-                id = letter.id,
-                content = letter.content,
-                receptionId = letter.reception.id,
-                senderId = letter.sender.id,
-                isDelivered = letter.createdAt.plusHours(12).isBefore(LocalDateTime.now()),
-                createdAt = letter.createdAt
-            )
+        if (letters.isEmpty()) {
+            throw LetterNotSendException
         }
+
+        return letterBoxRepository.findAllBySender(user)
+            .filter { it.sender.id == user.id }
+            .map { letter ->
+                LetterSentListResponse(
+                    id = letter.id,
+                    content = letter.content,
+                    receptionId = letter.reception.id,
+                    senderId = letter.sender.id,
+                    isDelivered = letter.createdAt.plusHours(12).isBefore(LocalDateTime.now()),
+                    createdAt = letter.createdAt
+                )
+            }
     }
 }
