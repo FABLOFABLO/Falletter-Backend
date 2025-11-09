5d90a4760ad5a31b858e76849a55ae5d0f211ddf
eeuunn <haspring@dsm.hs.kr>
2025-11-09 23:20:47 +0900

    response id 반환

--- src/main/kotlin/com/example/falletterbackend/falletter/dto/user/response/UserGetAllStudentResponse.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/dto/user/response/UserGetAllStudentResponse.kt
@@ -1,6 +1,7 @@
 package com.example.falletterbackend.falletter.dto.user.response
 
 data class UserGetAllStudentResponse(
+    val id: Long,
     val schoolNumber: String,
     val name: String
 )
--- src/main/kotlin/com/example/falletterbackend/falletter/dto/user/response/UserInfoResponse.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/dto/user/response/UserInfoResponse.kt
@@ -3,6 +3,7 @@ package com.example.falletterbackend.falletter.dto.user.response
 import com.example.falletterbackend.falletter.entity.user.Gender
 
 data class UserInfoResponse(
+    val id: Long,
     val email: String,
     val schoolNumber: String,
     val name: String,
