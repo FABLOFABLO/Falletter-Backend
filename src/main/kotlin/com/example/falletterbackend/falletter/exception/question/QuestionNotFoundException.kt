477fa1bea101277e5255833f1b1e56ad2d3c9d58
eeuunn <haspring@dsm.hs.kr>
2025-11-16 00:57:11 +0900

    Update QuestionNotFoundException.kt

--- src/main/kotlin/com/example/falletterbackend/falletter/exception/question/QuestionNotFoundException.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/exception/question/QuestionNotFoundException.kt
@@ -3,4 +3,4 @@ package com.example.falletterbackend.falletter.exception.question
 import com.example.falletterbackend.common.error.exception.ErrorCode
 import com.example.falletterbackend.common.error.exception.FalletterException
 
-class QuestionNotFoundException : FalletterException(ErrorCode.QUESTION_NOT_FOUND)
+object QuestionNotFoundException : FalletterException(ErrorCode.QUESTION_NOT_FOUND)
