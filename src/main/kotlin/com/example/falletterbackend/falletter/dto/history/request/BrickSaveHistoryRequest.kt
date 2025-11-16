0857e6f7423bd2d94b35f0a66e65280810062ace
eeuunn <haspring@dsm.hs.kr>
2025-11-16 21:06:18 +0900

    Update BrickSaveHistoryRequest.kt

--- src/main/kotlin/com/example/falletterbackend/falletter/dto/history/request/BrickSaveHistoryRequest.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/dto/history/request/BrickSaveHistoryRequest.kt
@@ -3,7 +3,7 @@ package com.example.falletterbackend.falletter.dto.history.request
 import com.example.falletterbackend.falletter.entity.history.HistoryType
 import com.example.falletterbackend.falletter.entity.question.Question
 
-data class BrickUsedHistoryRequest(
+data class BrickSaveHistoryRequest(
     val title: String,
     val description: String,
     val amount: Long,
