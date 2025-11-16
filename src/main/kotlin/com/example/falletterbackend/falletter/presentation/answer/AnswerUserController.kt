e35bb4fbadd80f7cd72dfa39b57853403460cf2a
eeuunn <haspring@dsm.hs.kr>
2025-11-16 21:40:39 +0900

    Update AnswerUserController.kt

--- src/main/kotlin/com/example/falletterbackend/falletter/presentation/answer/AnswerUserController.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/presentation/answer/AnswerUserController.kt
@@ -5,7 +5,7 @@ import com.example.falletterbackend.falletter.dto.answer.response.AnswerUserChos
 import com.example.falletterbackend.falletter.dto.history.response.BrickUsedHistoryResponse
 import com.example.falletterbackend.falletter.presentation.RestApiSpec
 import com.example.falletterbackend.falletter.service.answer.AnswerUserChosenService
-import com.example.falletterbackend.falletter.service.answer.AnswerUserHistoryService
+import com.example.falletterbackend.falletter.service.history.BrickUsedHistoryService
 import com.example.falletterbackend.falletter.service.answer.AnswerUserService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
@@ -20,19 +20,12 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/answer")
 class AnswerUserController(
     private val answerUserService: AnswerUserService,
-    private val answerUserHistoryService: AnswerUserHistoryService,
     private val answerUserChosenService: AnswerUserChosenService
 ) {
     @PostMapping(RestApiSpec.ANSWER_POST)
     @ResponseStatus(HttpStatus.CREATED)
     fun answerPost(@RequestBody @Valid request: AnswerUserRequest) {
         answerUserService.execute(request)
-    }
-
-    @GetMapping(RestApiSpec.ANSWER_USED)
-    @ResponseStatus(HttpStatus.OK)
-    fun answerUsed(): List<BrickUsedHistoryResponse> {
-        return answerUserHistoryService.execute()
     }
 
     @GetMapping(RestApiSpec.ANSWER_PICK)
