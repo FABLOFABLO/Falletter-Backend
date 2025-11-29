746d07287f07c5a4bf4a6e82cbb20aac65103652
eeuunn <haspring@dsm.hs.kr>
2025-11-29 19:19:27 +0900

    Create FilterConfig.kt

---
+++ src/main/kotlin/com/example/falletterbackend/common/config/FilterConfig.kt
@@ -0,0 +1,25 @@
+package com.example.falletterbackend.common.config
+
+import com.example.falletterbackend.common.error.ExceptionFilter
+import com.example.falletterbackend.common.security.TokenFilter
+import com.example.falletterbackend.common.security.TokenProvider
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.stereotype.Component
+
+@Component
+class FilterConfig(
+    private val tokenProvider: TokenProvider,
+    private val objectMapper: ObjectMapper
+) : SecurityConfigurerAdapter<DefaultSecurityFilterChain?, HttpSecurity>() {
+
+    override fun configure(builder: HttpSecurity) {
+        val tokenFilter = TokenFilter(tokenProvider)
+        val exceptionFilter = ExceptionFilter(objectMapper)
+        builder.addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter::class.java)
+        builder.addFilterBefore(exceptionFilter, tokenFilter::class.java)
+    }
+}
