235f26c16c00b826461864b251db796428af67bb
유하은 <yooha797@gmail.com>
2025-12-22 17:17:07 +0900

    Update UserSignUpRequest.kt

--- src/main/kotlin/com/example/falletterbackend/falletter/dto/user/request/UserSignUpRequest.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/dto/user/request/UserSignUpRequest.kt
@@ -1,6 +1,7 @@
 package com.example.falletterbackend.falletter.dto.user.request
 
 import com.example.falletterbackend.falletter.entity.user.Gender
+import com.example.falletterbackend.falletter.entity.user.Theme
 
 data class UserSignUpRequest(
     val email: String,
@@ -8,5 +9,6 @@ data class UserSignUpRequest(
     val schoolNumber: String,
     val name: String,
     val gender: Gender,
+    val theme: Theme,
     val profileImage: String?
 )
