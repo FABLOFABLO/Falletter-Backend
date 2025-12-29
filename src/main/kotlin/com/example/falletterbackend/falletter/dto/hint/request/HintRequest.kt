cc2847eb6f2783f1acf69cd43f9cad755e7691bb
유하은 <yooha797@gmail.com>
2025-12-29 17:15:34 +0900

    feat(hint) : create dto

---
+++ src/main/kotlin/com/example/falletterbackend/falletter/dto/hint/request/HintRequest.kt
@@ -0,0 +1,7 @@
+package com.example.falletterbackend.falletter.dto.hint.request
+
+class HintRequest(
+    val first: String,
+    val second: String,
+    val third: String
+)
---
+++ src/main/kotlin/com/example/falletterbackend/falletter/dto/hint/response/HintResponse.kt
@@ -0,0 +1,9 @@
+package com.example.falletterbackend.falletter.dto.hint.response
+
+class HintResponse(
+    val id: String,
+    val first: String,
+    val second: String,
+    val third: String,
+    val userId: Long
+)
