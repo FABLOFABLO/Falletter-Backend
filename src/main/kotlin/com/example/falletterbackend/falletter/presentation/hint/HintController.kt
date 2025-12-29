b2c23266c6e311ebd30c5dde73f29e555af93ebc
eeuunn <haspring@dsm.hs.kr>
2025-12-29 21:11:13 +0900

    Update HintController.kt

--- src/main/kotlin/com/example/falletterbackend/falletter/presentation/hint/HintController.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/presentation/hint/HintController.kt
@@ -1,17 +1,20 @@
 package com.example.falletterbackend.falletter.presentation.hint
 
 import com.example.falletterbackend.falletter.dto.hint.request.HintSaveRequest
+import com.example.falletterbackend.falletter.dto.hint.request.HintUpdateRequest
 import com.example.falletterbackend.falletter.dto.hint.response.HintResponse
 import com.example.falletterbackend.falletter.presentation.RestApiSpec
 import com.example.falletterbackend.falletter.service.hint.HintGetService
 import com.example.falletterbackend.falletter.service.hint.HintSaveService
 import com.example.falletterbackend.falletter.service.hint.HintUpdateService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 
@@ -23,16 +26,20 @@ class HintController(
     private val hintUpdateService: HintUpdateService
 ) {
     @PostMapping(RestApiSpec.HINT_SAVE)
+    @ResponseStatus(HttpStatus.CREATED)
     fun saveHint(@RequestBody request: HintSaveRequest) {
         hintSaveService.execute(request)
     }
 
     @GetMapping(RestApiSpec.HINT_GET_ALL)
+    @ResponseStatus(HttpStatus.OK)
     fun getAllHint(@PathVariable("answer-id") id: Long): HintResponse {
         return hintGetService.execute(id)
     }
 
     @PatchMapping(RestApiSpec.HINT_UPDATE)
-    fun updateHint(@RequestBody request: HintSaveRequest) {
+    @ResponseStatus(HttpStatus.OK)
+    fun updateHint(@RequestBody request: HintUpdateRequest) {
+        hintUpdateService.execute(request)
     }
 }
