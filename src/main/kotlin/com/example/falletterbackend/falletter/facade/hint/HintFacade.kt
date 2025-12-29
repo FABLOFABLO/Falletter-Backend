f2533d774507774a946c16aa275ea87d0d56c908
eeuunn <haspring@dsm.hs.kr>
2025-12-29 21:11:03 +0900

    Create HintFacade.kt

---
+++ src/main/kotlin/com/example/falletterbackend/falletter/facade/hint/HintFacade.kt
@@ -0,0 +1,15 @@
+package com.example.falletterbackend.falletter.facade.hint
+
+import com.example.falletterbackend.falletter.entity.hint.Hint
+import com.example.falletterbackend.falletter.entity.hint.repository.HintRepository
+import com.example.falletterbackend.falletter.exception.hint.HintNotFoundException
+import org.springframework.stereotype.Component
+
+@Component
+class HintFacade(
+    private val hintRepository: HintRepository
+) {
+    fun getCurrentHint(id: Long): Hint {
+        return hintRepository.findById(id).orElseThrow { HintNotFoundException }
+    }
+}
