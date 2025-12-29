c5a23d064eb77ae0b34d25c21179dbb7eac3dd2f
eeuunn <haspring@dsm.hs.kr>
2025-12-29 21:11:17 +0900

    Update HintUpdateService.kt

--- src/main/kotlin/com/example/falletterbackend/falletter/service/hint/HintUpdateService.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/service/hint/HintUpdateService.kt
@@ -1,8 +1,16 @@
 package com.example.falletterbackend.falletter.service.hint
 
+import com.example.falletterbackend.falletter.dto.hint.request.HintUpdateRequest
+import com.example.falletterbackend.falletter.facade.hint.HintFacade
 import org.springframework.stereotype.Service
 
-
 @Service
-class HintUpdateService {
+class HintUpdateService(
+    private val hintFacade: HintFacade
+) {
+    fun execute(request: HintUpdateRequest) {
+        val hint = hintFacade.getCurrentHint(request.hintId)
+
+        hint.update(request.firstHint, request.secondHint, request.thirdHint)
+    }
 }
