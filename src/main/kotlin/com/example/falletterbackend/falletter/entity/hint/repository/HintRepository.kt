1fa54c43f21ca4b043e01586d41fd41b049a0ea2
eeuunn <haspring@dsm.hs.kr>
2025-12-29 20:24:04 +0900

    Update HintRepository.kt

--- src/main/kotlin/com/example/falletterbackend/falletter/entity/hint/repository/HintRepository.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/entity/hint/repository/HintRepository.kt
@@ -1,7 +1,10 @@
 package com.example.falletterbackend.falletter.entity.hint.repository
 
+import com.example.falletterbackend.falletter.dto.hint.response.HintResponse
 import com.example.falletterbackend.falletter.entity.hint.Hint
+import com.example.falletterbackend.falletter.entity.user.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface HintRepository : JpaRepository<Hint, Long> {
+    fun findByIdAndUser(id: Long, user: User): HintResponse
 }
