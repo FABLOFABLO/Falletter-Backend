8ca6064180d3e34b059a072eb3e35a78032f376f
유하은 <yooha797@gmail.com>
2025-12-29 17:53:07 +0900

    Update HintController.kt

--- src/main/kotlin/com/example/falletterbackend/falletter/presentation/hint/HintController.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/presentation/hint/HintController.kt
@@ -1,8 +1,15 @@
 package com.example.falletterbackend.falletter.presentation.hint
 
+import com.example.falletterbackend.falletter.dto.hint.request.HintRequest
+import com.example.falletterbackend.falletter.entity.hint.Hint
+import com.example.falletterbackend.falletter.presentation.RestApiSpec
 import com.example.falletterbackend.falletter.service.hint.HintGetService
 import com.example.falletterbackend.falletter.service.hint.HintSaveService
 import com.example.falletterbackend.falletter.service.hint.HintUpdateService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,4 +21,17 @@ class HintController(
     private val hintGetService: HintGetService,
     private val hintUpdateService: HintUpdateService
 ) {
+    @PostMapping(RestApiSpec.HINT_SAVE)
+    fun saveHint(@RequestBody request: HintRequest) {
+        hintSaveService.execute(request)
+    }
+
+    @GetMapping(RestApiSpec.HINT_GET_ALL)
+    fun getAllHint() {
+
+    }
+
+    @PatchMapping(RestApiSpec.HINT_UPDATE)
+    fun updateHint(@RequestBody request: HintRequest) {
+    }
 }
