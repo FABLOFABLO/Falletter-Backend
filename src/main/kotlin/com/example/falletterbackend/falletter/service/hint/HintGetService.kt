f795c1a31bc6d0885720d59c0fcab586142ca1c8
eeuunn <haspring@dsm.hs.kr>
2025-12-29 20:24:09 +0900

    Update HintGetService.kt

--- src/main/kotlin/com/example/falletterbackend/falletter/service/hint/HintGetService.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/service/hint/HintGetService.kt
@@ -1,7 +1,18 @@
 package com.example.falletterbackend.falletter.service.hint
 
+import com.example.falletterbackend.falletter.dto.hint.response.HintResponse
+import com.example.falletterbackend.falletter.entity.hint.repository.HintRepository
+import com.example.falletterbackend.falletter.facade.user.UserFacade
 import org.springframework.stereotype.Service
 
 @Service
-class HintGetService {
+class HintGetService(
+    private val userFacade: UserFacade,
+    private val hintRepository: HintRepository
+) {
+    fun execute(id: Long): HintResponse {
+        val user = userFacade.getCurrentUser()
+
+        return hintRepository.findByIdAndUser(id, user)
+    }
 }
