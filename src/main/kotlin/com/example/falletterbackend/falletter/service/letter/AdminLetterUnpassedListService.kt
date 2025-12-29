2b59e0d7245e96d1efb6766913370fc463bda517
eeuunn <haspring@dsm.hs.kr>
2025-12-30 02:19:27 +0900

    Update AdminLetterUnpassedListService.kt

--- src/main/kotlin/com/example/falletterbackend/falletter/service/letter/AdminLetterUnpassedListService.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/service/letter/AdminLetterUnpassedListService.kt
@@ -1,32 +1,26 @@
 package com.example.falletterbackend.falletter.service.letter
 
-import com.example.falletterbackend.falletter.dto.letter.response.LetterReceivedListResponse
+import com.example.falletterbackend.falletter.dto.letter.response.AdminLetterUnpassedListResponse
 import com.example.falletterbackend.falletter.entity.letter.repository.LetterRepository
 import com.example.falletterbackend.falletter.exception.letter.LetterNotFoundException
-import com.example.falletterbackend.falletter.facade.user.UserFacade
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.time.LocalDateTime
 
 @Service
 class AdminLetterUnpassedListService(
-    private val letterRepository: LetterRepository,
-    private val userFacade: UserFacade
+    private val letterRepository: LetterRepository
 ) {
     @Transactional(readOnly = true)
-    fun execute(): List<LetterReceivedListResponse> {
-        val user = userFacade.getCurrentUser()
-        val letters = letterRepository.findAllByReception_IdAndIsPassed(user.id, false)
+    fun execute(): List<AdminLetterUnpassedListResponse> {
+        val letters = letterRepository.findAllByIsPassed(false)
 
         if (letters.isEmpty()) {
             throw LetterNotFoundException
         }
 
         return letters.map {
-            LetterReceivedListResponse(
+            AdminLetterUnpassedListResponse(
                 id = it.id,
-                content = it.content,
-                isDelivered = it.createdAt.plusHours(12).isBefore(LocalDateTime.now()),
                 isPassed = it.isPassed,
                 receptionId = it.reception.id,
                 senderId = it.sender.id,
