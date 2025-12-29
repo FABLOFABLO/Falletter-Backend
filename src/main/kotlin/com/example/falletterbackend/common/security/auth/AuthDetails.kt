2e276f68a96d834c6ee792cf060ad99b9d327f3c
eeuunn <haspring@dsm.hs.kr>
2025-12-30 02:02:12 +0900

    Update AuthDetails.kt

--- src/main/kotlin/com/example/falletterbackend/common/security/auth/AuthDetails.kt
+++ src/main/kotlin/com/example/falletterbackend/common/security/auth/AuthDetails.kt
@@ -2,12 +2,14 @@ package com.example.falletterbackend.common.security.auth
 
 import com.example.falletterbackend.falletter.entity.user.User
 import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
 class AuthDetails(
     private val user: User
 ) : UserDetails {
-    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = mutableListOf()
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> =
+        mutableListOf(SimpleGrantedAuthority(user.role.key))
 
     override fun getPassword(): String? = null
 
