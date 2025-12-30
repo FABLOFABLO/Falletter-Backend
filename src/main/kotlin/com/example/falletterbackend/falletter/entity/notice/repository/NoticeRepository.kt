74c1b0e0d396e342172f0e380d7f495863a80a88
유하은 <yooha797@gmail.com>
2025-12-30 13:03:48 +0900

    Create NoticeRepository.kt

---
+++ src/main/kotlin/com/example/falletterbackend/falletter/entity/notice/repository/NoticeRepository.kt
@@ -0,0 +1,6 @@
+package com.example.falletterbackend.falletter.entity.notice.repository
+
+import com.example.falletterbackend.falletter.entity.notice.Notice
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface NoticeRepository : JpaRepository<Notice, Long>
