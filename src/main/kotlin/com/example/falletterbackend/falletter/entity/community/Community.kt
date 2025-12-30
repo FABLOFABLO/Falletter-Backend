c49837a39f800300cbc7bc44a05b14630d458210
유하은 <yooha797@gmail.com>
2025-12-30 14:46:26 +0900

    Update Community.kt

--- src/main/kotlin/com/example/falletterbackend/falletter/entity/community/Community.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/entity/community/Community.kt
@@ -14,6 +14,9 @@ class Community(
     @Column(name = "content", columnDefinition = "VARCHAR(512)", nullable = false)
     var content: String,
 
+    @Column(name = "is_deleted", nullable = false)
+    var isDeleted: Boolean = false,
+
     @OneToMany(mappedBy = "community", cascade = [CascadeType.ALL], orphanRemoval = true)
     val comments : MutableList<Comment> = mutableListOf(),
 
@@ -24,5 +27,9 @@ class Community(
     fun update(title: String, content: String){
         this.title = title
         this.content = content
+    }
+
+    fun delete() {
+        this.isDeleted = true
     }
 }
