b63198176ecbd3601af5a7f1a18b7d36419e0ce2
유하은 <yooha797@gmail.com>
2025-12-30 13:35:18 +0900

    create service

--- src/main/kotlin/com/example/falletterbackend/falletter/service/notice/NoticeCreateService.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/service/notice/NoticeCreateService.kt
@@ -1,8 +1,10 @@
 package com.example.falletterbackend.falletter.service.notice
 
+import com.example.falletterbackend.common.exception.AccessDeniedException
 import com.example.falletterbackend.falletter.dto.notice.request.NoticeCreateRequest
 import com.example.falletterbackend.falletter.entity.notice.Notice
 import com.example.falletterbackend.falletter.entity.notice.repository.NoticeRepository
+import com.example.falletterbackend.falletter.entity.user.enums.Role
 import com.example.falletterbackend.falletter.facade.user.UserFacade
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -15,6 +17,10 @@ class NoticeCreateService(
     @Transactional
     fun execute(request: NoticeCreateRequest) {
         val user = userFacade.getCurrentUser()
+
+        if (user.role != Role.ADMIN) {
+            throw AccessDeniedException
+        }
 
         val notice = Notice(
             title = request.title,
--- src/main/kotlin/com/example/falletterbackend/falletter/service/notice/NoticeDeleteService.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/service/notice/NoticeDeleteService.kt
@@ -1,17 +1,27 @@
 package com.example.falletterbackend.falletter.service.notice
 
+import com.example.falletterbackend.common.exception.AccessDeniedException
 import com.example.falletterbackend.falletter.entity.notice.repository.NoticeRepository
+import com.example.falletterbackend.falletter.entity.user.enums.Role
 import com.example.falletterbackend.falletter.exception.notice.NoticeNotFoundException
+import com.example.falletterbackend.falletter.facade.user.UserFacade
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class NoticeDeleteService(
-    private val noticeRepository: NoticeRepository
+    private val noticeRepository: NoticeRepository,
+    private val userFacade: UserFacade
 ) {
     @Transactional
     fun execute(noticeId: Long) {
+        val user = userFacade.getCurrentUser()
+
+        if (user.role != Role.ADMIN) {
+            throw AccessDeniedException
+        }
+
         val notice = noticeRepository.findByIdOrNull(noticeId)
             ?: throw NoticeNotFoundException
 
