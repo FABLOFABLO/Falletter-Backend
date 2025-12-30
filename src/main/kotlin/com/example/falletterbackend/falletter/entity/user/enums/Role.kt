493ae04a0e3323a09f024de7bf60cad42a248108
유하은 <yooha797@gmail.com>
2025-12-30 09:13:10 +0900

    Update Role.kt

--- src/main/kotlin/com/example/falletterbackend/falletter/entity/user/enums/Role.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/entity/user/enums/Role.kt
@@ -3,6 +3,6 @@ package com.example.falletterbackend.falletter.entity.user.enums
 enum class Role(
     val key: String
 ) {
-    USER("ROLE_USER"),
-    ADMIN("ROLE_ADMIN")
+    USER("USER"),
+    ADMIN("ADMIN")
 }
