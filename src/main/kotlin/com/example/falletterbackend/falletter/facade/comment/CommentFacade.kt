4b0ee2648434f9ee9fd08259a6d67f69c1a27b50
유하은 <yooha797@gmail.com>
2025-12-31 11:05:19 +0900

    feat(facade) : comment

---
+++ src/main/kotlin/com/example/falletterbackend/falletter/facade/comment/CommentFacade.kt
@@ -0,0 +1,26 @@
+package com.example.falletterbackend.falletter.facade.comment
+
+import com.example.falletterbackend.falletter.entity.comment.Comment
+import com.example.falletterbackend.falletter.entity.comment.repository.CommentRepository
+import com.example.falletterbackend.falletter.exception.comment.CommentNotFoundException
+import com.example.falletterbackend.falletter.exception.user.UserMismatchException
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+
+@Component
+class CommentFacade(
+    private val commentRepository: CommentRepository
+) {
+    fun getCommentById(id: Long): Comment {
+        return commentRepository.findByIdOrNull(id)
+            ?: throw CommentNotFoundException
+    }
+
+    fun getCommentByIdWithOwnerCheck(id: Long, userId: Long): Comment {
+        val comment = getCommentById(id)
+        if (comment.user.id != userId) {
+            throw UserMismatchException
+        }
+        return comment
+    }
+}
--- src/main/kotlin/com/example/falletterbackend/falletter/service/comment/CommentDeleteService.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/service/comment/CommentDeleteService.kt
@@ -1,24 +1,21 @@
 package com.example.falletterbackend.falletter.service.comment
 
 import com.example.falletterbackend.falletter.entity.comment.repository.CommentRepository
-import com.example.falletterbackend.falletter.exception.comment.CommentNotFoundException
-import com.example.falletterbackend.falletter.exception.user.UserMismatchException
+import com.example.falletterbackend.falletter.facade.comment.CommentFacade
 import com.example.falletterbackend.falletter.facade.user.UserFacade
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class CommentDeleteService(
     private val userFacade: UserFacade,
+    private val commentFacade: CommentFacade,
     private val commentRepository: CommentRepository
 ) {
     @Transactional
     fun execute(id: Long) {
         val user = userFacade.getCurrentUser()
-        val comment = commentRepository.findByIdOrNull(id) ?: throw CommentNotFoundException
-
-        if (user.id != comment.user.id) throw UserMismatchException
+        commentFacade.getCommentByIdWithOwnerCheck(id, user.id)
 
         commentRepository.deleteById(id)
     }
