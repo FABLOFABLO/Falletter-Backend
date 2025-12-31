2838156d4d1ce937e6bb6ecaa4ad48c0cd908b8d
유하은 <yooha797@gmail.com>
2025-12-31 10:11:53 +0900

    refactoring : hint

--- src/main/kotlin/com/example/falletterbackend/falletter/facade/hint/HintFacade.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/facade/hint/HintFacade.kt
@@ -3,6 +3,7 @@ package com.example.falletterbackend.falletter.facade.hint
 import com.example.falletterbackend.falletter.entity.hint.Hint
 import com.example.falletterbackend.falletter.entity.hint.repository.HintRepository
 import com.example.falletterbackend.falletter.exception.hint.HintNotFoundException
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 
 @Component
@@ -10,6 +11,6 @@ class HintFacade(
     private val hintRepository: HintRepository
 ) {
     fun getCurrentHint(id: Long): Hint {
-        return hintRepository.findById(id).orElseThrow { HintNotFoundException }
+        return hintRepository.findByIdOrNull(id) ?: throw HintNotFoundException
     }
 }
