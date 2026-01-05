26f132f662a51094166a89a10dee82dbd4a3aed4
유하은 <yooha797@gmail.com>
2026-01-05 10:33:12 +0900

    feat(notice) : notice facade 설정

---
+++ src/main/kotlin/com/example/falletterbackend/falletter/facade/notice/NoticeFacade.kt
@@ -0,0 +1,21 @@
+package com.example.falletterbackend.falletter.facade.notice
+
+import com.example.falletterbackend.falletter.entity.notice.Notice
+import com.example.falletterbackend.falletter.entity.notice.repository.NoticeRepository
+import com.example.falletterbackend.falletter.exception.notice.NoticeNotFoundException
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+
+@Component
+class NoticeFacade(
+    private val noticeRepository: NoticeRepository
+) {
+    fun getNoticeById(id: Long): Notice {
+        return noticeRepository.findByIdOrNull(id)
+            ?: throw NoticeNotFoundException
+    }
+
+    fun getAllNotices(): List<Notice> {
+        return noticeRepository.findAll()
+    }
+}
