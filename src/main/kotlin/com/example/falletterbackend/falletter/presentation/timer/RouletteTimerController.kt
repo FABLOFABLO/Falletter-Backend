d5cecdb150687da618a701da391511a092db2335
유하은 <yooha797@gmail.com>
2026-01-05 11:38:11 +0900

    feat(timer) : timer controller

---
+++ src/main/kotlin/com/example/falletterbackend/falletter/presentation/timer/RouletteTimerController.kt
@@ -0,0 +1,40 @@
+package com.example.falletterbackend.falletter.presentation.timer
+
+import com.example.falletterbackend.falletter.dto.roulette.response.RouletteTimerResponse
+import com.example.falletterbackend.falletter.presentation.RestApiSpec
+import com.example.falletterbackend.falletter.service.timer.RouletteTimerGetRemainingService
+import com.example.falletterbackend.falletter.service.timer.RouletteTimerStartService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@Tag(name = "Timer", description = "룰렛 타이머 API")
+@RestController
+@RequestMapping("/timer")
+class RouletteTimerController(
+    private val rouletteTimerStartService: RouletteTimerStartService,
+    private val rouletteTimerGetRemainingService: RouletteTimerGetRemainingService
+) {
+    @Operation(summary = "룰렛 타이머 시작", description = "24시간 타이머를 시작합니다.")
+    @ApiResponses(
+        ApiResponse(responseCode = "201", description = "타이머 시작 성공")
+    )
+    @PostMapping(RestApiSpec.TIMER_ROULETTE_POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    fun startTimer() {
+        rouletteTimerStartService.execute()
+    }
+
+    @Operation(summary = "룰렛 타이머 조회", description = "남은 시간(초)을 조회합니다.")
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "조회 성공")
+    )
+    @GetMapping(RestApiSpec.TIMER_ROULETTE_GET)
+    @ResponseStatus(HttpStatus.OK)
+    fun getRemainingTime(): RouletteTimerResponse {
+        return rouletteTimerGetRemainingService.execute()
+    }
+}
