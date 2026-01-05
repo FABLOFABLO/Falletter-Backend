9d4605cfbaec0aba29079188f5bdb2d2769658dc
유하은 <yooha797@gmail.com>
2026-01-05 15:26:59 +0900

    feat(notification) : facade

---
+++ src/main/kotlin/com/example/falletterbackend/falletter/facade/notification/NotificationFacade.kt
@@ -0,0 +1,29 @@
+package com.example.falletterbackend.falletter.facade.notification
+
+import com.example.falletterbackend.falletter.entity.notification.Notification
+import com.example.falletterbackend.falletter.entity.notification.repository.NotificationRepository
+import com.example.falletterbackend.falletter.entity.user.User
+import com.example.falletterbackend.falletter.exception.notification.NotificationNotFoundException
+import org.springframework.stereotype.Component
+
+@Component
+class NotificationFacade(
+    private val notificationRepository: NotificationRepository
+) {
+    fun getByUser(user: User): Notification {
+        return notificationRepository.findByUser(user) ?: throw NotificationNotFoundException
+    }
+
+    fun getOrCreateByUser(user: User): Notification {
+        return notificationRepository.findByUser(user)
+            ?: notificationRepository.save(Notification(user = user))
+    }
+
+    fun save(notification: Notification): Notification {
+        return notificationRepository.save(notification)
+    }
+
+    fun existsByUser(user: User): Boolean {
+        return notificationRepository.existsByUser(user)
+    }
+}
