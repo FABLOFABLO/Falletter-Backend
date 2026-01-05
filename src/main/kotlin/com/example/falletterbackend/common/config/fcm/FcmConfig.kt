a89d78ddf1d21366fd6529119290bfcfb4679563
유하은 <yooha797@gmail.com>
2026-01-05 17:13:05 +0900

    Update FcmConfig.kt

--- src/main/kotlin/com/example/falletterbackend/common/config/fcm/FcmConfig.kt
+++ src/main/kotlin/com/example/falletterbackend/common/config/fcm/FcmConfig.kt
@@ -4,22 +4,25 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.messaging.FirebaseMessaging
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.ClassPathResource
+import java.io.FileInputStream
 
 @Configuration
-class FcmConfig {
+class FcmConfig(
+
+    @Value("\${firebase.credentials.path}")
+    private val firebaseKeyPath: String
+
+) {
 
     @Bean
     fun firebaseApp(): FirebaseApp {
-        if (FirebaseApp.getApps().isNotEmpty()) {
-            return FirebaseApp.getInstance()
-        }
-
-        val resource = ClassPathResource("serviceAccountKey.json")
         val options = FirebaseOptions.builder()
-            .setCredentials(GoogleCredentials.fromStream(resource.inputStream))
+            .setCredentials(
+                GoogleCredentials.fromStream(FileInputStream(firebaseKeyPath))
+            )
             .build()
 
         return FirebaseApp.initializeApp(options)
