ec290bddeebb7badc4ea6c0731bb78a0dd4a7ecf
유하은 <yooha797@gmail.com>
2026-01-05 15:21:37 +0900

    feat(device) : ctrl

---
+++ src/main/kotlin/com/example/falletterbackend/falletter/presentation/device/DeviceTokenController.kt
@@ -0,0 +1,42 @@
+package com.example.falletterbackend.falletter.presentation.device
+
+import com.example.falletterbackend.falletter.dto.device.request.DeviceTokenRequest
+import com.example.falletterbackend.falletter.dto.device.response.DeviceTokenRegisterResponse
+import com.example.falletterbackend.falletter.presentation.RestApiSpec
+import com.example.falletterbackend.falletter.service.device.DeviceTokenDeleteService
+import com.example.falletterbackend.falletter.service.device.DeviceTokenRegisterService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@Tag(name = "fcm_device", description = "디바이스 토큰 API")
+@RestController
+@RequestMapping("/device")
+class DeviceTokenController(
+    private val deviceTokenRegisterService: DeviceTokenRegisterService,
+    private val deviceTokenDeleteService: DeviceTokenDeleteService
+) {
+    @Operation(summary = "디바이스 토큰 등록", description = "FCM 토큰을 등록합니다.")
+    @ApiResponses(
+        ApiResponse(responseCode = "201", description = "등록 성공")
+    )
+    @PostMapping(RestApiSpec.DEVICE_TOKEN_REGISTER)
+    @ResponseStatus(HttpStatus.CREATED)
+    fun registerToken(@RequestBody @Valid request: DeviceTokenRequest): DeviceTokenRegisterResponse {
+        return deviceTokenRegisterService.execute(request)
+    }
+
+    @Operation(summary = "디바이스 토큰 삭제", description = "FCM 토큰을 삭제합니다.")
+    @ApiResponses(
+        ApiResponse(responseCode = "204", description = "삭제 성공")
+    )
+    @DeleteMapping(RestApiSpec.DEVICE_TOKEN_DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteToken(@PathVariable("device-id") deviceId: Long) {
+        deviceTokenDeleteService.execute(deviceId)
+    }
+}
