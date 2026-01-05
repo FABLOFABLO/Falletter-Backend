7c6c01210023e63d32257da475dd7fa6dd007c54
유하은 <yooha797@gmail.com>
2026-01-05 10:34:06 +0900

    feat(notice) : create notice get details service

---
+++ src/main/kotlin/com/example/falletterbackend/falletter/service/notice/NoticeGetDetailService.kt
@@ -0,0 +1,24 @@
+package com.example.falletterbackend.falletter.service.notice
+
+import com.example.falletterbackend.falletter.dto.notice.response.NoticeDetailsResponse
+import com.example.falletterbackend.falletter.facade.notice.NoticeFacade
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class NoticeGetDetailService(
+    private val noticeFacade: NoticeFacade
+) {
+    @Transactional(readOnly = true)
+    fun execute(id: Long): NoticeDetailsResponse {
+        val notice = noticeFacade.getNoticeById(id)
+
+        return NoticeDetailsResponse(
+            id = notice.id,
+            title = notice.title,
+            content = notice.content,
+            authorName = notice.author.name,
+            createdAt = notice.createdAt
+        )
+    }
+}
