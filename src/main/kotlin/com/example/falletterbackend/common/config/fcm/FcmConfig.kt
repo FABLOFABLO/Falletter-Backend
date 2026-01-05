91818e06f65b32122985fdce8220b7f577a9acb4
유하은 <yooha797@gmail.com>
2026-01-05 16:41:41 +0900

    Update FcmConfig.kt

--- src/main/kotlin/com/example/falletterbackend/common/config/fcm/FcmConfig.kt
+++ src/main/kotlin/com/example/falletterbackend/common/config/fcm/FcmConfig.kt
@@ -4,30 +4,21 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.messaging.FirebaseMessaging
-import jakarta.annotation.PostConstruct
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.io.File
-import java.io.IOException
 
 @Configuration
 class FcmConfig {
-    @PostConstruct
-    fun initialize() {
-        try {
-            if (FirebaseApp.getApps().isEmpty()) {
-                val options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(File(PATH).inputStream()))
-                    .build()
-                FirebaseApp.initializeApp(options)
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-    }
 
     @Bean
     fun firebaseMessaging(): FirebaseMessaging {
+        if (FirebaseApp.getApps().isEmpty()) {
+            val options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(File(PATH).inputStream()))
+                .build()
+            FirebaseApp.initializeApp(options)
+        }
         return FirebaseMessaging.getInstance()
     }
 
