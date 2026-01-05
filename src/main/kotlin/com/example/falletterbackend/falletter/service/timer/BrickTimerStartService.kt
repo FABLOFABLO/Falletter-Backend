9b1381405645cdf25225143dd6760b5f022fd7da
유하은 <yooha797@gmail.com>
2026-01-05 11:34:10 +0900

    feat(timer) : brick timer start service

---
+++ src/main/kotlin/com/example/falletterbackend/falletter/service/timer/BrickTimerStartService.kt
@@ -0,0 +1,23 @@
+package com.example.falletterbackend.falletter.service.timer
+
+import com.example.falletterbackend.common.utils.redis.RedisUtils
+import com.example.falletterbackend.falletter.facade.user.UserFacade
+import org.springframework.stereotype.Service
+import java.time.Duration
+
+@Service
+class BrickTimerStartService(
+    private val userFacade: UserFacade,
+    private val redisUtils: RedisUtils
+) {
+    companion object {
+        const val BRICK_TIMER_KEY_PREFIX = "brick_timer:"
+        private val TIMER_DURATION = Duration.ofHours(5)
+    }
+
+    fun execute() {
+        val user = userFacade.getCurrentUser()
+        val key = "$BRICK_TIMER_KEY_PREFIX${user.id}"
+        redisUtils.setValues(key, "active", TIMER_DURATION)
+    }
+}
