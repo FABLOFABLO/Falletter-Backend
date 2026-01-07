bfcd9cba6dbaaf75ac2b8093f208a37e3eafb3c1
유하은 <yooha797@gmail.com>
2026-01-07 12:39:36 +0900

    feat(hint) : exception

--- src/main/kotlin/com/example/falletterbackend/falletter/exception/hint/HintNotFoundException.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/exception/hint/HintNotFoundException.kt
@@ -3,4 +3,4 @@ package com.example.falletterbackend.falletter.exception.hint
 import com.example.falletterbackend.common.error.exception.ErrorCode
 import com.example.falletterbackend.common.error.exception.FalletterException
 
-object HintNotFoundException : FalletterException(ErrorCode.HISTORY_NOT_FOUND)
+object HintNotFoundException : FalletterException(ErrorCode.HINT_NOT_FOUND)
