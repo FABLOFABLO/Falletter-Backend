ef36e8dd93447c9bd3b3e68940e51f2a6226eb71
유하은 <yooha797@gmail.com>
2026-01-07 14:23:03 +0900

    feat(letter) : exception

--- src/main/kotlin/com/example/falletterbackend/falletter/exception/letter/LetterNotSentException.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/exception/letter/LetterNotSentException.kt
@@ -3,4 +3,4 @@ package com.example.falletterbackend.falletter.exception.letter
 import com.example.falletterbackend.common.error.exception.ErrorCode
 import com.example.falletterbackend.common.error.exception.FalletterException
 
-object LetterNotSendException : FalletterException(ErrorCode.LETTER_NOT_SEND)
+object LetterNotSentException : FalletterException(ErrorCode.LETTER_NOT_SENT)
