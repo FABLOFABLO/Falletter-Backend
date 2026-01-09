43e9ff4f58d771b0583d23c36404331946f43f02
유하은 <yooha797@gmail.com>
2026-01-09 11:26:55 +0900

    chore(s3) : s3 경로 상수화

--- src/main/kotlin/com/example/falletterbackend/falletter/service/image/ImageUploadService.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/service/image/ImageUploadService.kt
@@ -2,15 +2,17 @@ package com.example.falletterbackend.falletter.service.image
 
 import com.example.falletterbackend.common.utils.s3.S3Utils
 import com.example.falletterbackend.falletter.dto.image.response.ImageUploadResponse
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 
 @Service
 class ImageUploadService(
-    private val s3Utils: S3Utils
+    private val s3Utils: S3Utils,
+    @Value("\${cloud.aws.s3.profile-path}") private val profilePath: String
 ) {
     fun execute(file: MultipartFile): ImageUploadResponse {
-        val imageUrl = s3Utils.upload(file, "falletter/profile")
+        val imageUrl = s3Utils.upload(file, profilePath)
         return ImageUploadResponse(imageUrl)
     }
 }
