d06b2c93b0a8e05dd861ec646c08e98bd3c006f2
유하은 <yooha797@gmail.com>
2026-01-09 13:28:02 +0900

    chore : yml

--- src/main/kotlin/com/example/falletterbackend/falletter/service/image/ImageUploadService.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/service/image/ImageUploadService.kt
@@ -2,17 +2,15 @@ package com.example.falletterbackend.falletter.service.image
 
 import com.example.falletterbackend.common.utils.s3.S3Utils
 import com.example.falletterbackend.falletter.dto.image.response.ImageUploadResponse
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 
 @Service
 class ImageUploadService(
-    private val s3Utils: S3Utils,
-    @Value("\${cloud.aws.s3.profile-path}") private val profilePath: String
+    private val s3Utils: S3Utils
 ) {
     fun execute(file: MultipartFile): ImageUploadResponse {
-        val imageUrl = s3Utils.upload(file, profilePath)
+        val imageUrl = s3Utils.upload(file, "falletter/profile")
         return ImageUploadResponse(imageUrl)
     }
 }
