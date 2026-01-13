6d637ab47035f17932e68ace025a1e63717ed1e5
유하은 <yooha797@gmail.com>
2026-01-13 17:13:16 +0900

    refactoring : history

--- src/main/kotlin/com/example/falletterbackend/falletter/service/history/BrickSaveHistoryService.kt
+++ src/main/kotlin/com/example/falletterbackend/falletter/service/history/BrickSaveHistoryService.kt
@@ -2,11 +2,9 @@ package com.example.falletterbackend.falletter.service.history
 
 import com.example.falletterbackend.falletter.dto.history.request.BrickSaveHistoryRequest
 import com.example.falletterbackend.falletter.entity.history.History
-import com.example.falletterbackend.falletter.entity.question.repository.QuestionRepository
-import com.example.falletterbackend.falletter.exception.question.QuestionNotFoundException
 import com.example.falletterbackend.falletter.facade.history.HistoryFacade
+import com.example.falletterbackend.falletter.facade.question.QuestionFacade
 import com.example.falletterbackend.falletter.facade.user.UserFacade
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -14,14 +12,13 @@ import org.springframework.transaction.annotation.Transactional
 class BrickSaveHistoryService(
     private val historyFacade: HistoryFacade,
     private val userFacade: UserFacade,
-    private val questionRepository: QuestionRepository
+    private val questionFacade: QuestionFacade
 ) {
     @Transactional
     fun execute(request: BrickSaveHistoryRequest) {
         val targetUser = userFacade.getUserById(request.targetUserId)
         val writerUser = userFacade.getUserById(request.writerUserId)
-        val question = questionRepository.findByIdOrNull(request.questionId)
-            ?: throw QuestionNotFoundException
+        val question = questionFacade.findById(request.questionId)
 
         val history = History(
             title = request.title,
