78161ed64c6f58571d1e7a96aa83e76a97452d89
유하은 <yooha797@gmail.com>
2026-01-14 12:59:54 +0900

    Create AdminRole.kt

---
+++ src/main/kotlin/com/example/falletterbackend/falletter/entity/admin/enums/AdminRole.kt
@@ -0,0 +1,8 @@
+package com.example.falletterbackend.falletter.entity.admin.enums
+
+enum class AdminRole(
+    val key: String
+) {
+    ADMIN("ROLE_ADMIN"),
+    SUPER_ADMIN("ROLE_SUPER_ADMIN")
+}
